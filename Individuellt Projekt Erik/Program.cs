0d83fe5339b843c15769c592e0838060ee113252
Erik9507 <eriknorell1995@gmail.com>
2021-10-13 12:31:43 +0200

    Add project files.

---
+++ Individuellt Projekt Erik/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;   //Erik Norell SUT21
+using System.Threading;
+
+namespace Individuellt_Projekt_Erik
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
